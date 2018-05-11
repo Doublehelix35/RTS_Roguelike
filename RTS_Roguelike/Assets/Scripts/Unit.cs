18d8332f3a49ce1d908ae102c32db88026cb425b
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-05-11 15:05:24 +0100

    Unit code
    Started working on unit code and movement

---
+++ RTS_Roguelike/Assets/Scripts/Unit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    Vector3 TargetDestination;
+    float Speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        TargetDestination = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // Move
+        if (transform.position != TargetDestination)
+        {
+            // Calculate direction to head
+            Vector3 Dir = TargetDestination - transform.position;
+            Dir.z = 0;
+
+            // Move in direction
+            transform.Translate(Dir * Speed * Time.deltaTime);
+        }
+	}
+    
+    // Set new target to move this unit to
+    void SetTargetDestination(Vector3 NewDestination)
+    {
+        TargetDestination = NewDestination;
+    }
+}
