f94c41a9f7daacd30b754432dd04425e86548417
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-05-21 14:25:10 +0100

    Panning and scrolling
    Can pan camera up, down, left and right
    Can scroll to zoom in and out

--- RTS_Roguelike/Assets/Scripts/GameManager.cs
+++ RTS_Roguelike/Assets/Scripts/GameManager.cs
@@ -7,6 +7,15 @@ public class GameManager : MonoBehaviour {
     public GameObject BoxSelectorPrefab;
     GameObject BoxSelector;
     private bool BoxSelectionStarted = false;
+    float PanPercentageLowMin = 0.08f;
+    float PanPercentageLowMax = 0.15f;
+    float PanPercentageHighMin = 0.85f;
+    float PanPercentageHighMax = 0.92f;
+    float PanSpeed = 2.2f;
+
+    float ScrollSpeed = 15f;
+    float ScrollClampMin = 4f;
+    float ScrollClampMax = 25f;
 
     void Start ()
     {
@@ -17,6 +26,95 @@ public class GameManager : MonoBehaviour {
 	
 
 	void Update ()
+    {
+        // Panning camera
+        PanCamera();
+
+        // Scrolling
+        Scroll();
+
+        // Clicks
+        ClickChecks();
+        
+    }
+
+    void PanCamera()
+    {
+        // Left pan
+        if (Input.mousePosition.x < Screen.width * PanPercentageLowMax) 
+        {
+            if (Input.mousePosition.x < Screen.width * PanPercentageLowMin) // Fast pan
+            {
+                Camera.main.transform.position += Vector3.left * PanSpeed * Time.deltaTime * 3;
+            }
+            else // Normal speed
+            {
+                Camera.main.transform.position += Vector3.left * PanSpeed * Time.deltaTime;
+            }
+
+        }
+        // Right pan
+        else if (Input.mousePosition.x > Screen.width * PanPercentageHighMin)
+        {
+            if (Input.mousePosition.x > Screen.width * PanPercentageHighMax) // Fast pan
+            {
+                Camera.main.transform.position += Vector3.right * PanSpeed * Time.deltaTime * 3;
+            }
+            else // Normal speed
+            {
+                Camera.main.transform.position += Vector3.right * PanSpeed * Time.deltaTime;
+            }
+
+        }
+
+        // Down pan
+        if (Input.mousePosition.y < Screen.height * PanPercentageLowMax)
+        {
+            if (Input.mousePosition.y < Screen.height * PanPercentageLowMin) // Fast pan
+            {
+                Camera.main.transform.position += Vector3.back * PanSpeed * Time.deltaTime * 3;
+            }
+            else // Normal speed
+            {
+                Camera.main.transform.position += Vector3.back * PanSpeed * Time.deltaTime;
+            }
+
+        }
+        // Up pan
+        else if (Input.mousePosition.y > Screen.height * PanPercentageHighMin)
+        {
+            if (Input.mousePosition.y > Screen.height * PanPercentageHighMax) // Fast pan
+            {
+                Camera.main.transform.position += Vector3.forward * PanSpeed * Time.deltaTime * 3;
+            }
+            else // Normal speed
+            {
+                Camera.main.transform.position += Vector3.forward * PanSpeed * Time.deltaTime;
+            }
+
+        }
+
+    }
+
+    void Scroll()
+    {
+        // Scroll in
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) 
+        {
+            Camera.main.transform.position += Vector3.down * ScrollSpeed * Time.deltaTime;
+        }
+        // Scroll out
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Camera.main.transform.position += Vector3.up * ScrollSpeed * Time.deltaTime;
+        }
+
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, 
+                                                     Mathf.Clamp(Camera.main.transform.position.y, ScrollClampMin, ScrollClampMax), 
+                                                     Camera.main.transform.position.z);
+    }
+
+    void ClickChecks()
     {
         // Left click
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -38,7 +136,7 @@ public class GameManager : MonoBehaviour {
                     GetComponent<UnitManager>().AddUnitToSelection(hit.transform.gameObject);
                     Debug.Log(hit.transform.gameObject.name + " selected");
                 }
-            }            
+            }
         }
         // Left click & control
         else if (Input.GetKey(KeyCode.Mouse0) && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)))
@@ -48,7 +146,7 @@ public class GameManager : MonoBehaviour {
             BoxSelector.SetActive(true);
 
             if (BoxSelectionStarted)
-            { 
+            {
                 // Check for floor               
                 RaycastHit hit;
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -58,7 +156,7 @@ public class GameManager : MonoBehaviour {
                     if (hit.collider != null && hit.collider.gameObject.tag == "Floor")
                     {
                         // Edit box size
-                        Vector3 BoxSize = BoxSelector.transform.position - hit.point ;
+                        Vector3 BoxSize = BoxSelector.transform.position - hit.point;
                         BoxSelector.transform.GetChild(0).transform.localScale = BoxSize;
 
                         // Edit box position
@@ -87,7 +185,7 @@ public class GameManager : MonoBehaviour {
                         BoxSelector.transform.position = hit.point + new Vector3(0f, 1f, 0f);
                         BoxSelectionStarted = true;
                     }
-                }                
+                }
             }
 
         }
@@ -106,7 +204,7 @@ public class GameManager : MonoBehaviour {
                     {
                         GetComponent<UnitManager>().MoveSelection(hit.point);
                     }
-                }                
+                }
             }
             else
             {
