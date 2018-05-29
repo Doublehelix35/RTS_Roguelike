9d102f171eaabe9accc316791c7513ff1c07f7bf
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-05-29 22:14:29 +0100

    Basic menus added
    Basic menus added and functionality
    Scenes renamed

---
+++ RTS_Roguelike/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadScene(string SceneToLoad)
+    {
+        // Opens game scene
+        SceneManager.LoadScene(SceneToLoad);
+    }
+    
+    public void ExitGame()
+    {
+        // Closes game
+        Application.Quit();
+    }
+
+}
